75a0486952578796cc59d74c7a9b7b3b7d184c70
tlytlh <1535271281@qq.com>
2020-04-05 18:52:59 +0800

    修复除学生类型用户进入测试界面时也会插入数据的bug（在线测试只对学生用户开放）

--- src/main/resources/static/js/daojishi.js
+++ src/main/resources/static/js/daojishi.js
@@ -1,14 +1,19 @@
 var userId = document.getElementById("userId").innerText;
 var userName = document.getElementById("userName").innerText;
 var recordId = "";
-$.ajax({
-    type: "POST",
-    url:'/testQuestion/checkTestResult',
-    data: {"userId":userId, "userName":userName},
-    dataType:'json',
-    async: false,
-    success:function(data) {
-        document.getElementById("leftTime").innerHTML = data.leftTime;
-        recordId = data.recordId;
-    }
-});
+var checkRolePermission = document.getElementById("checkThat").innerText;
+if(checkRolePermission!='2'){
+    window.location.href="/myError"
+}else {
+    $.ajax({
+        type: "POST",
+        url:'/testQuestion/checkTestResult',
+        data: {"userId":userId, "userName":userName},
+        dataType:'json',
+        async: false,
+        success:function(data) {
+            document.getElementById("leftTime").innerHTML = data.leftTime;
+            recordId = data.recordId;
+        }
+    });
+}
