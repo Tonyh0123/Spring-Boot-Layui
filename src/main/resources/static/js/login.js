16f8fa2973b573d504aa274e6508cd994e53c041
tlytlh <1535271281@qq.com>
2020-05-08 13:44:24 +0800

    登录添加验证码； 页面样式修改

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,3 +1,12 @@
+
+var show_num = [];
+$(function () {
+    draw(show_num);
+    $("#canvas").on('click',function () {
+        draw(show_num);
+        console.log(show_num);
+    })
+});
 /**
  * 登录
  */
@@ -21,14 +30,80 @@ $(function(){
 function login(){
     var username=$("#username").val();
     var password=$("#password").val();
-    var rememberMe = $("#rememberMe").val();
-    $.post("/user/login",$("#useLogin").serialize(),function(data){
-        if(data.code == 1){
-            window.location.href=data.url;
-        }else{
-            layer.alert(data.message,function(){
-                layer.closeAll();//关闭所有弹框
-            });
-        }
-    });
+    // var rememberMe = $("#rememberMe").val();
+    var verifyCode=$("#verifyCode").val().toLowerCase();
+    var num = show_num.join("");
+    if(verifyCode == ''){
+        layer.msg('请输入验证码',{icon: 7, time: 2000});
+    }else if(verifyCode == num){
+        $.post("/user/login",$("#useLogin").serialize(),function(data){
+            if(data.code == 1){
+                window.location.href=data.url;
+            }else{
+                layer.alert(data.message,function(){
+                    layer.closeAll();//关闭所有弹框
+                });
+            }
+        });
+    }else {
+        layer.msg('验证码输入错误，请重新输入', {icon: 5, time:2000});
+        $("#verifyCode").val('');
+        draw(show_num);
+    }
+
+}
+
+//varifyCode
+function draw(show_num) {
+    var canvas_width = $('#canvas').width();
+    var canvas_height = $('#canvas').height();
+    var canvas = document.getElementById("canvas");//获取到canvas的对象，演员
+    var context = canvas.getContext("2d");//获取到canvas画图的环境，演员表演的舞台
+    canvas.width = canvas_width;
+    canvas.height = canvas_height;
+    var sCode = "A,B,C,E,F,G,H,J,K,L,M,N,P,Q,R,S,T,W,X,Y,Z,1,2,3,4,5,6,7,8,9,0";
+    var aCode = sCode.split(",");
+    var aLength = aCode.length;//获取到数组的长度
+
+    for (var i = 0; i <= 3; i++) {
+        var j = Math.floor(Math.random() * aLength);//获取到随机的索引值
+        var deg = Math.random() * 30 * Math.PI / 180;//产生0~30之间的随机弧度
+        var txt = aCode[j];//得到随机的一个内容
+        show_num[i] = txt.toLowerCase();
+        var x = 10 + i * 20;//文字在canvas上的x坐标
+        var y = 20 + Math.random() * 8;//文字在canvas上的y坐标
+        context.font = "bold 23px 微软雅黑";
+
+        context.translate(x, y);
+        context.rotate(deg);
+
+        context.fillStyle = randomColor();
+        context.fillText(txt, 0, 0);
+
+        context.rotate(-deg);
+        context.translate(-x, -y);
+    }
+    for (var i = 0; i <= 5; i++) { //验证码上显示线条
+        context.strokeStyle = randomColor();
+        context.beginPath();
+        context.moveTo(Math.random() * canvas_width, Math.random() * canvas_height);
+        context.lineTo(Math.random() * canvas_width, Math.random() * canvas_height);
+        context.stroke();
+    }
+    for (var i = 0; i <= 30; i++) { //验证码上显示小点
+        context.strokeStyle = randomColor();
+        context.beginPath();
+        var x = Math.random() * canvas_width;
+        var y = Math.random() * canvas_height;
+        context.moveTo(x, y);
+        context.lineTo(x + 1, y + 1);
+        context.stroke();
+    }
+}
+
+function randomColor() {//得到随机的颜色值
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + "," + g + "," + b + ")";
 }
